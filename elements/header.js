156e5226d39c9c16f09a1b1ccd2249c1b5a35e4e
Michael Szolowicz <102772395+MichaelSzolowicz@users.noreply.github.com>
2026-08-22 10:01:36 -0700

    Better organization for home page styling.

--- elements/header.js
+++ elements/header.js
@@ -6,40 +6,29 @@ headerTemplate.innerHTML = `
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 header {
-    width: 100%;
-    height: auto;
-
     background-color: var(--header-color);
-
+    width: var(--section-width);
+    height: fit-content;
     display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: left;
+    justify-content: left;
+    gap: 1em;
+    margin: 1em auto;
     margin-bottom: var(--default-bottom-margin);
 }
 
 .header-body {
-    width: var(--section-width);
-    max-width: var(--section-max-width);
 
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-
-    margin: 1em auto;
 }
 
 </style>
+
 <header>
-    <div class="header-body">
-        <div class="project-buttons">
-            <div><a href="index.html">Home</a></div>
-            <div><a href="media/resumeMichaelSzolowicz.pdf" target="blank">CV</a></div>
-            <div><a href="index.html#contact">Contact</a></div>
-        </div>
-    </div>
+    <div><a href="index.html">Home</a></div>
+    <div><a href="media/resumeMichaelSzolowicz.pdf" target="blank">CV</a></div>
+    <div><a href="index.html#contact">Contact</a></div>
 </header>
 `;
 
